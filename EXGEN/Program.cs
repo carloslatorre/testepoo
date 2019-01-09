12bdd4bea23218c51637bdfc1ecc139d3b507082
Carlos Latorre <carlos.latorre@hotmail.com>
2019-01-09 13:39:12 -0200

    mudei nome da booleana

--- EXGEN/Program.cs
+++ EXGEN/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp8
             int[,] matriz;
              
             
-            bool encontrou;
+            bool encontrouValor;
 
             string[] vet0 = Console.ReadLine().Split(' ');            N = ushort.Parse(vet0[0]);
             M = ushort.Parse(vet0[1]);
@@ -32,13 +32,13 @@ namespace ConsoleApp8
             }
                 X = 0;
               Y = 0;
-                encontrou = false;
+                encontrouValor = false;
              
 
-                for (int i = 1; i < N-1 && encontrou == false ; i++)
+                for (int i = 1; i < N-1 && encontrouValor == false ; i++)
                 {
 
-                    for (int j = 1; j < M-1 && encontrou == false ; j++)
+                    for (int j = 1; j < M-1 && encontrouValor == false ; j++)
                     {
                         if (matriz[i, j] == 42 && matriz[i - 1, j - 1] == 7 && matriz[i + 1, j + 1] == 7 &&
                             matriz[i - 1, j + 1] == 7 && matriz[i + 1, j - 1] == 7 && matriz[i, j + 1] == 7 &&
@@ -47,7 +47,7 @@ namespace ConsoleApp8
 
                             X = i + 1;
                             Y = j + 1;
-                            encontrou = true;
+                            encontrouValor = true;
                         }
 
 
